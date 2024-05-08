4283c721f46722f362f5d9555c049c3b38cf8f67
gabimezze <gabrielagomescezar@gmail.com>
2024-05-08 17:16:04 -0300

    Co-authored-by: Jennifer Suzuki <jenniesuzuki@users.noreply.github.com> Co-authored-by: ChavesChaves <ChavesChaves@users.noreply.github.com> js add e estilo atualizado

---
+++ js/script.js
@@ -0,0 +1,10 @@
+    window.watsonAssistantChatOptions = {
+    integrationID: "26e70437-eaa1-448e-bcf9-ba11660abc82",
+    region: "au-syd",
+    serviceInstanceID: "4224444c-a45c-4c8b-83a2-633cd08f9bfd",
+    onLoad: async (instance) => { await instance.render(); }
+    };
+    setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);});
